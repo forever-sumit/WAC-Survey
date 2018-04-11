c67b1617ac0b3c2a85607498bb928c67fe2bcff2
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-11 17:13:07 +0530

    added pagination and create select question page

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,15 @@
 class SurveysController < ApplicationController
 
-	def new
-		
-	end
+  def new
+    sections = Section.all
+    @sections = sections.paginate(:page => params[:page], :per_page => 1).order('in_order ASC')
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
 end
